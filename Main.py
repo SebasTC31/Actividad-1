59d919662dca7e28aa78a3ef2ab35802c9e56e5b
Sebastian Torres <juansebas0464@gmail.com>
2024-09-05 21:32:09 -0500

    Archivo Main.py modificado para mostrar fecha y hora

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
+from datetime import datetime
+
+def obtener_fecha_hora_actual():
+    ahora = datetime.now()
+
+    fecha_hora_formateada = ahora.strftime("%Y-%m-%d %H:%M:%S")
+    return fecha_hora_formateada
+
 print("Hello, World!")
+print(obtener_fecha_hora_actual())
